875b5878d10ef5d853ed99032262e98e72a3ea03
“GitJackub” <jakubxp9248@gmail.com>
2025-06-25 20:33:47 +0200

    adjust to new vessel

--- LevelMeasurement.py
+++ LevelMeasurement.py
@@ -1,59 +1,37 @@
 import cv2
 import numpy as np
 
-def nothing(x):
-    pass
-
 def measure_level(use_glass_image):
     # Należy dobrać parametry w zależności od obrazu i naczynia
-    # ------------------
-    top_cut = 80       # obcięcie konturu od góry
-    bottom_cut = 136   # obcięcie konturu od dołu
-    top_thresh = 255   # górny próg detekcji krawędzi
-    bottom_thresh = 50 # dolny próg detekcji krawędzi
-    # ------------------
+    # -----------------------
+    top_cut = 50            # obcięcie konturu od góry
+    bottom_cut = 44         # obcięcie konturu od dołu
+    top_thresh = 255        # górny próg detekcji krawędzi
+    bottom_thresh = 32      # dolny próg detekcji krawędzi
+    left_cut = 723          # obcięcie obrazu od lewej
+    right_cut = 191         # obcięcie obrazu od prawej
+    detection_start = 242   # dodatkowe obcięcie od dołu obrazu, aby uniknąć detekcji dna naczynia
+    # -----------------------
     if not use_glass_image:
         cap = cv2.VideoCapture(0)
         ret, img = cap.read()
         if not ret:
             return -1.0
     else:
-        img = cv2.imread('test1.jpg')
+        img = cv2.imread('test4.jpg')
         
     if img is None:
         return -2.0
     
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    
+    vessel = img_gray.copy()
+    w,h = img.shape[1], img.shape[0]
+    vessel = vessel[top_cut:h-bottom_cut, 0:w]
 
-    lower_black = np.array([0, 0, 0])
-    upper_black = np.array([180, 255, 60])
-    thresh = cv2.inRange(img_hsv, lower_black, upper_black)
+    vessel_height, vessel_width = vessel.shape
 
-    kernel = np.ones((5, 5), np.uint8)
-    thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
-    thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
-
-    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    if len(contours) == 0:
-        return 0
-
-    glass_contour = max(contours, key=cv2.contourArea)
-    x, y, w, h = cv2.boundingRect(glass_contour)
-
-    mask = np.zeros_like(img_gray, dtype=np.uint8)
-    cv2.drawContours(mask, [glass_contour], -1, 255, -1)
-
-    roi = cv2.bitwise_and(img_gray, img_gray, mask=mask)
-
-    roi = roi[y+top_cut:y+h-bottom_cut, x:x+w]
-
-    if roi.size == 0:
-        return 0
-
-    roi_height, roi_width = roi.shape
-
+    roi = vessel[0:h-detection_start, left_cut:vessel_width-right_cut]
     sobel_y = cv2.Sobel(roi, cv2.CV_64F, 0, 1, ksize=3)
     sobel_y = cv2.convertScaleAbs(sobel_y)
     _, edge_thresh = cv2.threshold(sobel_y, bottom_thresh, top_thresh, cv2.THRESH_BINARY)
@@ -61,8 +39,7 @@ def measure_level(use_glass_image):
     histogram = np.sum(edge_thresh, axis=1)
     line_y = np.argmax(histogram)
 
-    fill = round((roi_height - line_y) / roi_height, 2)
-    if fill < 0 or fill > 1:
-        fill = -0.03
+    fill = round((vessel_height - line_y) / vessel_height, 2) * 100
+    y_line_global =  line_y + top_cut
         
-    return fill * 100
+    return fill, y_line_global
